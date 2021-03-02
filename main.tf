36c628fce5f9e631c13502ea612da1e8074ddeb8
trvsmtchll <trvsmtchll@yahoo.com>
2021-03-01 21:10:01 -0500

    cleanup

--- main.tf
+++ main.tf
@@ -71,27 +71,3 @@ resource "random_password" "password" {
   override_special = "_%@"
 }
 
-/*
-#ssh
-resource "tls_private_key" "main" {
-  algorithm = "RSA"
-}
-
-resource "null_resource" "main" {
-  provisioner "local-exec" {
-    command = "echo \"${tls_private_key.main.private_key_pem}\" > ./priv-key.pem"
-  }
-
-  provisioner "local-exec" {
-    command = "echo \"${tls_private_key.main.public_key_openssh}\" > ./id_rsa.pub"
-  }
-
-  provisioner "local-exec" {
-    command = "chmod 600 ./priv-key.pem"
-  }
-
-  provisioner "local-exec" {
-    command = "chmod 600 ./id_rsa.pub"
-  }
-}*/
-
